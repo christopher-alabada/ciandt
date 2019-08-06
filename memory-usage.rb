401ba5867dce9dfc61bbe7dec4abb8abde79c6ce
Christopher Alabada <christopher.alabada@gmail.com>
2019-08-06 15:43:01 +0900

    Added more comments

--- memory-usage.rb
+++ memory-usage.rb
@@ -5,42 +5,51 @@ def memoryUsage(totalMemory, foregroundApps, backgroundApps)
   return "Error: totalMemory must be postitive Integer" if totalMemory < 1
 
 
-  memRanking = []
+  # All possible pairs
+  memoryPairings = []
 
+  # Optimal pair apps
+  optimalPair = []
 
+  # Loop through foreground and backgound app
   foregroundApps.each do |foreground_app|
     backgroundApps.each do |backgound_app|
+      # add total memory used
       totalMemoryUsed = foreground_app[1] + backgound_app[1]
-      if totalMemoryUsed > totalMemory
+
+      # Append to memoryPairings only if totalMemoryUsed is less than or equal to totalMemory
+      if totalMemoryUsed <= totalMemory
         next
       end
-      memRanking << [[foreground_app[0], backgound_app[0]], totalMemoryUsed]
+      memoryPairings << [[foreground_app[0], backgound_app[0]], totalMemoryUsed]
     end
   end
 
-  memRanking.sort! {|a,b| b[1] <=> a[1]}
-  # p memRanking
+  # Reverse sort memoryPairings
+  memoryPairings.sort! {|a,b| b[1] <=> a[1]}
 
-  optimalPair = []
-  memRanking.each do |app|
+  # Place all app pairings into optimalPair if it equals totalMemory
+  memoryPairings.each do |app|
     optimalPair << app[0] if app[1] == totalMemory
   end
 
+  # if optimal pair doesn't equal totalMemory, just get the first one since it's reversed sorted
   if optimalPair.count == 0
-    optimalPair << memRanking[0][0]
+    optimalPair << memoryPairings[0][0]
   end
 
+  # Return just the pair if there's only one pairing
   if optimalPair.count == 1
     optimalPair = optimalPair[0]
   end
   
+  # Done
   return optimalPair
 end
 
 
+# Some tests
 p memoryUsage('10', [[1,2], [2,5], [3,8]], [[1,4]])
 p memoryUsage(-10, [[1,2], [2,5], [3,8]], [[1,4]])
 p memoryUsage(10, [[1,2], [2,5], [3,8]], [[1,4]])
 p memoryUsage(20, [[1,10], [2,15], [3,17]], [[1,3], [2,5]])
-
-
